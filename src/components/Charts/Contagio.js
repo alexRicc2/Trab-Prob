8d0155405e7e09cc716b1dc34779209f42900b95
deigo-lps <diego.lopes0909@gmail.com>
2022-01-11 14:57:36 -0300

    pronto2?

--- src/components/Charts/Contagio.js
+++ src/components/Charts/Contagio.js
@@ -3,39 +3,37 @@ import Rform from "../Forms/Rform";
 import Chart from "../Chart";
 
 //gera valor aleatório na distribuição Poisson com base na média R
-const handlePush = (R,aux) =>{
-  var L=Math.exp(-R);
-  var p=1;
-  var k=0;
+const handlePush = (R, aux) => {
+  var L = Math.exp(-R);
+  var p = 1;
+  var k = 0;
 
-  do{
+  do {
     k++;
-    p*=Math.random();
-  }while(p>L)
+    p *= Math.random();
+  } while (p > L);
 
-  for(var i=0;i<k-1;i++){
+  for (var i = 0; i < k - 1; i++) {
     aux.push(0);
   }
   // console.log(k-1);
-}
-
-
+};
 
 export default function Contagio() {
   const [dados, setDados] = useState([]);
   //i=meses, j=dias
-  const handleSubmit = (R, T, recupera) => {
+  const handleSubmit = (R,T) => {
     var plot = [];
     var aux = Array(10).fill(0);
-    for (var i = 1; i <= 11; i++) {
+    for (var i = 1; i <= 12; i++) {
       if (aux.length > 100000) break;
       for (var j = 1; j <= 30; j++) {
         // console.log(aux.length);
         if (aux.length > 100000) break;
         for (var index = 0; index < aux.length; index++) {
           aux[index]++;
-          if (aux[index] >= recupera) {
-            handlePush(R,aux);
+          if (aux[index] >= T) {
+            handlePush(R, aux);
             aux.splice(index, 1);
           }
         }
@@ -44,7 +42,6 @@ export default function Contagio() {
     }
     setDados([{ label: "Qtd. Infectados", data: plot }]);
   };
-
 
   return (
     <div>
--- src/components/Forms/Rform.js
+++ src/components/Forms/Rform.js
@@ -3,7 +3,6 @@ import React,{useState} from "react";
 export default function Rform(props){
   const [T,setT]=useState("");
   const [R,setR]=useState("");
-  const [recupera,setRecupera]=useState("");
 
   const handleR = (event) =>{
     setR(event.target.value)
@@ -11,13 +10,10 @@ export default function Rform(props){
   const handleT = (event) =>{
     setT(event.target.value)
   }
-  const handleRecupera = (event) =>{
-    setRecupera(event.target.value)
-  }
 
   const handleSubmit = (event)=>{
     event.preventDefault();
-    props.onSubmit(R,T,recupera);
+    props.onSubmit(R,T);
   }
 
 
@@ -27,8 +23,6 @@ export default function Rform(props){
       <input type="number" step="0.0001" onChange={handleR} id="r"></input>
       <label htmlFor="t">T</label>
       <input type="number" step="0.0001" onChange={handleT} id="t"></input>
-      <label htmlFor="recupera">recupera</label>
-      <input type="number" step="0.0001" onChange={handleRecupera} id="recupera"></input>
       <button type="submit">submit</button>
     </form>
   )
