119fe9df85bd067f31aa947b312f2b6d7891f6d3
deigo-lps <diego.lopes0909@gmail.com>
2022-01-11 12:51:12 -0300

    n ta funfando mas sipa q funfa

--- src/components/Charts/Contagio.js
+++ src/components/Charts/Contagio.js
@@ -3,27 +3,56 @@ import Rform from "../Forms/Rform";
 import Chart from "../Chart";
 export default function Contagio() {
   const [dados, setDados] = useState([]);
+  //i=meses, j=dias
+  const handleSubmit = (R, T, recupera) => {
+    var plot=[];
+    var aux=Array(10).fill(0)
+    // aux.push(5,5);
+    // aux=aux.map(element=>element+1);
+    
+    
+    for (var i = 1; i <= 12; i++) {
+      for (var j = 1; j <= 30; j++) {
+
+
+        for(var index=0;index<aux.length;index++){
+          aux[index]++;
+          if(aux[index]>=recupera){
+            aux.push(0,0);
+            aux.splice(index,i);
+            console.log(aux);
+          }
+        }
+
+
+      }
+      plot.push([i, aux.length]);
+    }
+
+    setDados([{ label: "Qtd. Infectados", data: plot }]);
+
+  };
 
   //a cada duas semanas se recupera.
-  const handleSubmit = (R, T) => {
-    var aux = [];
-    var Total = [];
-    Total[1] = 1;
-    for (var i = 1; i <= 12; i++) {
-      if (i > 1) Total[i] = Total[i - 1];
-      for (var j = 0; j < 30; j += T) {
-        Total[i] = Total[i] + Total[i] * R;
-        console.log(i,Total[i]);
-      }
-      aux.push([i, Total[i]]);
-    }
-    setDados([{ label: "Qtd. Infectados", data: aux }]);
-    console.log(aux);
-  };
+  // const handleSubmit = (R, T) => {
+  //   var aux = [];
+  //   var Total = [];
+  //   Total[1] = 1;
+  //   for (var i = 1; i <= 12; i++) {
+  //     if (i > 1) Total[i] = Total[i - 1];
+  //     for (var j = 0; j < 30; j += T) {
+  //       Total[i] = Total[i] + Total[i] * R;
+  //       console.log(i,Total[i]);
+  //     }
+  //     aux.push([i, Total[i]]);
+  //   }
+    // setDados([{ label: "Qtd. Infectados", data: aux }]);
+  //   console.log(aux);
+  // };
 
   return (
     <div>
-      <Chart data={dados} type="line"/>
+      <Chart data={dados} type="line" />
       <Rform onSubmit={handleSubmit}></Rform>
     </div>
   );
--- src/components/Forms/Rform.js
+++ src/components/Forms/Rform.js
@@ -3,6 +3,7 @@ import React,{useState} from "react";
 export default function Rform(props){
   const [T,setT]=useState("");
   const [R,setR]=useState("");
+  const [recupera,setRecupera]=useState("");
 
   const handleR = (event) =>{
     setR(event.target.value)
@@ -10,10 +11,13 @@ export default function Rform(props){
   const handleT = (event) =>{
     setT(event.target.value)
   }
+  const handleRecupera = (event) =>{
+    setRecupera(event.target.value)
+  }
 
   const handleSubmit = (event)=>{
     event.preventDefault();
-    props.onSubmit(R,T);
+    props.onSubmit(R,T,recupera);
   }
 
 
@@ -23,6 +27,8 @@ export default function Rform(props){
       <input type="number" onChange={handleR} id="r"></input>
       <label htmlFor="t">T</label>
       <input type="number" onChange={handleT} id="t"></input>
+      <label htmlFor="recupera">recupera</label>
+      <input type="number" onChange={handleRecupera} id="recupera"></input>
       <button type="submit">submit</button>
     </form>
   )
