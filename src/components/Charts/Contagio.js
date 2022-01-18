6f0d1a8d8a1f8832ea885ce46c955c12ebe2488e
deigo-lps <diego.lopes0909@gmail.com>
2022-01-18 18:12:46 -0300

    total de infectados

--- src/components/Charts/Contagio.js
+++ src/components/Charts/Contagio.js
@@ -7,6 +7,7 @@ const handlePush = (R, aux) => {
   var L = Math.exp(-R);
   var p = 1;
   var k = 0;
+  var total=0;
 
   do {
     k++;
@@ -15,11 +16,14 @@ const handlePush = (R, aux) => {
 
   for (var i = 0; i < k - 1; i++) {
     aux.push(0);
+    total++;
   }
+  return total
   // console.log(k-1);
 };
 
 export default function Contagio() {
+  var total=10;
   const [dados, setDados] = useState([]);
   //i=meses, j=dias
   const handleSubmit = (R,T) => {
@@ -33,7 +37,7 @@ export default function Contagio() {
         for (var index = 0; index < aux.length; index++) {
           aux[index]++;
           if (aux[index] >= T) {
-            handlePush(R, aux);
+            total+=handlePush(R, aux);
             aux.splice(index, 1);
           }
         }
@@ -41,6 +45,7 @@ export default function Contagio() {
       plot.push([i, aux.length]);
     }
     setDados([{ label: "Qtd. Infectados", data: plot }]);
+    console.log("total de infectados: "+total)
   };
 
   return (
